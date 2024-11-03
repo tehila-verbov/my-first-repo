a605f5eb2a1c7063dca3128118252b74e6d3e564
tehila-verbov <tehila.hubara@gmail.com>
2024-11-03 15:38:26 +0200

    Update main.py
    added from programmer 2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Employee import Employee
 student = Student("Mike", 22, "Engeneering", 1, 80)
 # student.foo()
 
+print("This is from programmer 2")
 employee = Employee("Tehila", 40, "Software Engineer", 45000)
 people = [student, employee]
 for person in people:
