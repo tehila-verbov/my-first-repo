16112c1fe6e1a4c1507f1616ec114f76f054b903
Tehila Verbov <tehila.hubara@gmail.com>
2024-11-03 14:34:36 +0200

    updated avg

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Mike", 22, "Engeneering", 1, 70)
+student = Student("Mike", 22, "Engeneering", 1, 80)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
