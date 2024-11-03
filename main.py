700fbc6f207c367ef02efb89db4c15c5e73b36e8
tehila-verbov <tehila.hubara@gmail.com>
2024-11-03 16:23:00 +0200

    Update main.py
    adding print prom programmer 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Student import Student
 from Employee import Employee
 
 student = Student("Mike", 22, "Engeneering", 1, 80)
-print("This is from programmer 1, this is from programmer 2")
+print("This is from programmer 1, this is from programmer 3")
 # student.foo()
 
 print("This is from programmer 2")
